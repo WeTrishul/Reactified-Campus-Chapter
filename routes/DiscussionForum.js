01233b2584751b3da36a0af5d855d214cb28958d
realshivam <realshivam1201@gmail.com>
2021-07-02 22:27:13 +0530

    adding more authorization checks in Discussion feature for improving the security

--- routes/DiscussionForum.js
+++ routes/DiscussionForum.js
@@ -6,12 +6,12 @@ const DiscussController = require('../controllers/DiscussionForum')
 
 router.get('/Discuss',DiscussController.discuss)
 
-router.post('/postit',DiscussController.postit)
+router.post('/postit',passport.checkAuthentication,DiscussController.postit)
 
-router.post('/commentit',DiscussController.commentit)
+router.post('/commentit',passport.checkAuthentication,DiscussController.commentit)
 
-router.get('/destroypost/:id',DiscussController.deletepost)
+router.get('/destroypost/:id',passport.checkAuthentication,DiscussController.deletepost)
 
-router.get('/destroycomment/:id',DiscussController.deletecomment)
+router.get('/destroycomment/:id',passport.checkAuthentication,DiscussController.deletecomment)
 
 module.exports = router
