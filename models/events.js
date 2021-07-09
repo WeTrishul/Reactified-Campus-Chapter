081bbbd046088e93fd04f708fe45d5085a460931
realshivam <realshivam1201@gmail.com>
2021-07-09 16:44:02 +0530

    adding create events button and events schema

---
+++ models/events.js
@@ -0,0 +1,42 @@
+const mongoose=require('mongoose')
+const db = require('../config/db')
+
+
+const EventSchema = new  moongose.Schema({
+
+    creatorid : {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+
+    eventname : {
+        type:String,
+        required:true
+    },
+
+    aboutevent:{
+        type:String,
+        required:true
+    },
+
+    eventStartTime:{
+        type:String,
+        required:true
+    },
+    eventEndTime : {
+        type:String,
+        required:true
+    },
+    eventDate : {
+        type:Date,
+        required:true
+    },
+
+    Registeredusers : [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    }]
+
+},{
+    timestamps:true
+})
