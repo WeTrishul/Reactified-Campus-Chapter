abed85f97d7976f9ddfc2a7400520691ca495c59
Anand-Choudhary <anand.c.rishra@gmail.com>
2022-02-19 13:42:53 +0530

    Enhanced User Experience 2

--- Client/src/Components/Page/AllEvents/UpComingEvents.jsx
+++ Client/src/Components/Page/AllEvents/UpComingEvents.jsx
@@ -7,6 +7,14 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CreateIcon from '@mui/icons-material/Create';
 import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
+import Paper from '@mui/material/Paper';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import GroupsIcon from '@mui/icons-material/Groups';
+import ReplyIcon from '@mui/icons-material/Reply';
 import { Link } from 'react-router-dom';
 import './Events.css';
 
@@ -22,16 +30,317 @@ const style = {
   boxShadow: 24,
   p: 4,
 };
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  borderRadius: '20px',
+  color: theme.palette.text.secondary,
+}));
 function UpComingEvents() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const eventsRenderingOnWidth = () => {
+    if (window.innerWidth <= 431) {
+      return (
+        <Box>
+          <Box>
+            <Box sx={{ width: 1 }}>
+              <Box display='grid' gridTemplateColumns='repeat(12, 1fr)' gap={2}>
+                <Box gridColumn='span 12'>
+                  <Item sx={{ height: '70vh' }}>
+                    <Box sx={{ display: 'flex' }}>
+                      <Box>
+                        <GroupsIcon sx={{ fontSize: '30px' }} />
+                      </Box>
+                      <Box sx={{ marginLeft: '2px' }}>120</Box>
+                      <Box sx={{ display: 'flex', marginLeft: '60px' }}>
+                        <Box>
+                          <EditIcon />
+                        </Box>
+                        <Box sx={{ marginLeft: '20px' }}>
+                          <DeleteIcon />
+                        </Box>
+                        <Box sx={{ marginLeft: '20px' }}>
+                          <ReplyIcon />
+                        </Box>
+                      </Box>
+                    </Box>
+                    <Box sx={{ width: '100%' }}>
+                      <img
+                        style={{
+                          width: '100%',
+                          height: '200px',
+                          borderRadius: '20px',
+                        }}
+                        src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+                        alt=''
+                      />
+                    </Box>
+                    <Box>
+                      <Typography
+                        sx={{ fontWeight: '600', fontSize: '15px' }}
+                        variant='h7'
+                      >
+                        UPCOMING EVENT HEADING
+                      </Typography>
+                    </Box>
+                    <Box sx={{ fontSize: '10px' }}>
+                      College Event |{' '}
+                      <AccessTimeFilledIcon sx={{ paddingTop: '8px' }} /> 08:00
+                    </Box>
+                    <Box
+                      sx={{
+                        marginTop: '20px',
+                        textAlign: 'center',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      <Button variant='contained' color='success'>
+                        Register
+                      </Button>
+                    </Box>
+                  </Item>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      );
+    } else if (window.innerWidth >= 431 && window.innerWidth < 768) {
+      return (
+        <Box>
+          <Box sx={{ width: 1, marginTop: '1rem' }}>
+            <Box display='grid' gridTemplateColumns='repeat(12, 1fr)' gap={2}>
+              <Box gridColumn='span 12'>
+                <Item sx={{ height: '30vh' }}>
+                  <Box sx={{ display: 'flex' }}>
+                    <Box sx={{ maxWidth: '30%' }}>
+                      <img
+                        style={{
+                          width: '100%',
+                          height: '140px',
+                          borderRadius: '20px',
+                        }}
+                        src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+                        alt=''
+                      />
+                    </Box>
+                    <Box sx={{ width: '50%' }}>
+                      <Box sx={{ marginLeft: '20px' }}>
+                        <Box>
+                          <Typography sx={{ fontWeight: '600' }} variant='h7'>
+                            UPCOMING EVENT HEADING
+                          </Typography>
+                        </Box>
+                        <Box>
+                          College Event |{' '}
+                          <AccessTimeFilledIcon sx={{ paddingTop: '8px' }} />{' '}
+                          08:00
+                        </Box>
+
+                        <Box sx={{ marginTop: '5px' }}>
+                          <Box>Starts 19 feb </Box>
+                        </Box>
+                      </Box>
+                    </Box>
+                    <Box sx={{ marginLeft: '20px' }}>
+                      <Box sx={{ display: 'flex', marginLeft: '30px' }}>
+                        <Box>
+                          <GroupsIcon sx={{ fontSize: '30px' }} />
+                        </Box>
+                        <Box sx={{ marginLeft: '2px' }}>120</Box>
+                      </Box>
+                      <Box sx={{ marginTop: '20px' }}>
+                        <Box sx={{ display: 'flex' }}>
+                          <Box>
+                            <EditIcon />
+                          </Box>
+                          <Box sx={{ marginLeft: '20px' }}>
+                            <DeleteIcon />
+                          </Box>
+                          <Box sx={{ marginLeft: '20px' }}>
+                            <ReplyIcon />
+                          </Box>
+                        </Box>
+                      </Box>
+                      <Box sx={{ marginTop: '20px' }}>
+                        <Button variant='contained' color='success'>
+                          Register
+                        </Button>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Item>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      );
+    } else if (window.innerWidth >= 768 && window.innerWidth < 1000) {
+      return (
+        <Box>
+          <Box sx={{ width: 1, marginTop: '1rem' }}>
+            <Box display='grid' gridTemplateColumns='repeat(12, 1fr)' gap={2}>
+              <Box gridColumn='span 12'>
+                <Item sx={{ height: '30vh' }}>
+                  <Box sx={{ display: 'flex' }}>
+                    <Box sx={{ maxWidth: '30%' }}>
+                      <img
+                        style={{
+                          width: '100%',
+                          height: '140px',
+                          borderRadius: '20px',
+                        }}
+                        src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+                        alt=''
+                      />
+                    </Box>
+                    <Box sx={{ width: '50%' }}>
+                      <Box sx={{ marginLeft: '20px' }}>
+                        <Box>
+                          <Typography sx={{ fontWeight: '600' }} variant='h6'>
+                            UPCOMING EVENT HEADING
+                          </Typography>
+                        </Box>
+                        <Box>
+                          COLLEGE EVENT |{' '}
+                          <AccessTimeFilledIcon sx={{ paddingTop: '8px' }} />{' '}
+                          08:00
+                        </Box>
+
+                        <Box sx={{ marginTop: '10px' }}>
+                          <Box>
+                            Starts 19 feb{' '}
+                            <AccessTimeFilledIcon sx={{ paddingTop: '8px' }} />
+                            08:00
+                          </Box>
+                        </Box>
+                      </Box>
+                    </Box>
+                    <Box sx={{ marginLeft: '20px' }}>
+                      <Box sx={{ display: 'flex', marginLeft: '30px' }}>
+                        <Box>
+                          <GroupsIcon sx={{ fontSize: '30px' }} />
+                        </Box>
+                        <Box sx={{ marginLeft: '2px' }}>120</Box>
+                      </Box>
+                      <Box sx={{ marginTop: '20px' }}>
+                        <Box sx={{ display: 'flex' }}>
+                          <Box>
+                            <EditIcon />
+                          </Box>
+                          <Box sx={{ marginLeft: '20px' }}>
+                            <DeleteIcon />
+                          </Box>
+                          <Box sx={{ marginLeft: '20px' }}>
+                            <ReplyIcon />
+                          </Box>
+                        </Box>
+                      </Box>
+                      <Box sx={{ marginTop: '20px' }}>
+                        <Button variant='contained' color='success'>
+                          Register
+                        </Button>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Item>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      );
+    } else if (window.innerWidth > 1000) {
+      return (
+        <Box>
+          <Box sx={{ width: 1, marginTop: '1rem' }}>
+            <Box display='grid' gridTemplateColumns='repeat(12, 1fr)' gap={2}>
+              <Box gridColumn='span 12'>
+                <Item sx={{ height: '30vh' }}>
+                  <Box sx={{ display: 'flex' }}>
+                    <Box sx={{ maxWidth: '20%' }}>
+                      <img
+                        style={{
+                          width: '100%',
+                          height: '165px',
+                          borderRadius: '20px',
+                        }}
+                        src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
+                        alt=''
+                      />
+                    </Box>
+                    <Box sx={{ width: '60%' }}>
+                      <Box sx={{ marginLeft: '20px' }}>
+                        <Box>
+                          <Typography sx={{ fontWeight: '600' }} variant='h6'>
+                            UPCOMING EVENT HEADING
+                          </Typography>
+                        </Box>
+                        <Box>
+                          COLLEGE EVENT |{' '}
+                          <AccessTimeFilledIcon sx={{ paddingTop: '8px' }} />{' '}
+                          08:00
+                        </Box>
+                        <Box sx={{ marginTop: '10px' }}>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. A perferendis cupiditate vitae ex, quo nihil?
+                          Ullam error magnam possimus, quos quod repudiandae
+                          iste, iure, alias autem sequi unde praesentium non.
+                        </Box>
+                        <Box sx={{ marginTop: '5px' }}>
+                          <Box>
+                            Starts 19 feb{' '}
+                            <AccessTimeFilledIcon sx={{ paddingTop: '8px' }} />
+                            08:00
+                          </Box>
+                        </Box>
+                      </Box>
+                    </Box>
+                    <Box sx={{ marginLeft: '20px' }}>
+                      <Box sx={{ display: 'flex', marginLeft: '30px' }}>
+                        <Box>
+                          <GroupsIcon sx={{ fontSize: '30px' }} />
+                        </Box>
+                        <Box sx={{ marginLeft: '2px' }}>120</Box>
+                      </Box>
+                      <Box sx={{ marginTop: '20px' }}>
+                        <Box sx={{ display: 'flex' }}>
+                          <Box>
+                            <EditIcon />
+                          </Box>
+                          <Box sx={{ marginLeft: '20px' }}>
+                            <DeleteIcon />
+                          </Box>
+                          <Box sx={{ marginLeft: '20px' }}>
+                            <ReplyIcon />
+                          </Box>
+                        </Box>
+                      </Box>
+                      <Box sx={{ marginTop: '20px' }}>
+                        <Button variant='contained' color='success'>
+                          Register
+                        </Button>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Item>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      );
+    }
+  };
   return (
     <div>
       <div className='upComingEvents-Body-Box'>
         <div className='upComingEvents-Outer-Box'>
           <div className='upComingEvents-Inner-Box'>
             <div>
+              <div>{eventsRenderingOnWidth()}</div>
               {/* <Button onClick={handleOpen}>Open modal</Button> */}
               <Modal
                 aria-labelledby='transition-modal-title'
